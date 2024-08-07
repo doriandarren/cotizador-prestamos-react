27c9d01bfabbc2dbbcf086d7336b0fc643398696
Dorian Gonzalez <doriandarren1@gmail.com>
2024-08-07 22:19:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        
+        <Header />
+
+      </div>
+    </>
+  )
+}
+
+export default App
