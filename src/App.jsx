a0e18cb00ff17d0501c3e8330f3e6be9aa02f5e3
Dorian Gonzalez <doriandarren1@gmail.com>
2024-08-08 22:02:49 +0200

    component button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import { useState } from 'react';
 import Header from "./components/Header";
+import Button from "./components/Button";
+import { formatearDinero } from './helpers';
+
 
 
 
 function App() {
+
+  const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+
+  function handleChange(e){
+    setCantidad(Number(e.target.value));
+  }
+
+
+  function handleClickDecremento() {
+    const valor = cantidad - STEP;
+    if(valor < MIN){
+      alert('Cantidad no válida');
+      return;
+    }
+    setCantidad(valor);
+  }
+
+
+  function handleClickIncremento() {
+    const valor = cantidad + STEP;
+    if(valor > MAX){
+      alert('Cantidad no válida');
+      return;
+    }
+    setCantidad(valor);
+  }
+
 
 
   return (
@@ -10,6 +47,49 @@ function App() {
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
         
         <Header />
+
+        <div className='flex justify-between my-6'>
+
+          <Button 
+            operador='-'
+            fn={handleClickDecremento}
+          />
+
+          <Button
+            operador='+'
+            fn={handleClickIncremento}
+          />
+
+        </div>
+
+        <input 
+          type="range" 
+          className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+          onChange={handleChange}
+          min={MIN}
+          max={MAX}
+          step={STEP}
+          value={cantidad}
+        />
+
+        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{ formatearDinero(cantidad) }</p>
+
+        <h2 className="text 2xl font-extrabold text-gray-500 text-center">
+          Elige un <span className='text-indigo-600'>Plazo</span> a pagar
+        </h2>
+
+        <select
+          className="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-cl font-bold text-gray-500 mt-5"
+          value={meses}
+          onChange={e => setMeses(+e.target.value)}
+        >
+          <option value="6">6 Meses</option>
+          <option value="12">12 Meses</option>
+          <option value="24">24 Meses</option>
+        </select>
+
+
+
 
       </div>
     </>
---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+const formatearDinero = (valor) => {
+    const formatter = new Intl.NumberFormat('es-ES', {
+        style: 'currency',
+        currency: 'EUR'
+    });
+    return formatter.format(valor);
+}
+
+export {
+    formatearDinero
+}
